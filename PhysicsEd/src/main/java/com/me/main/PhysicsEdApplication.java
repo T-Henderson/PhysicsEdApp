a80da3f7bdebc4f6ea6ee1b5ee2a9fcfe28a86ad
e-Henderson <ethenderson15@gmail.com>
2021-09-09 18:57:17 -0400

    Beginning of WebApp backend

---
+++ PhysicsEd/src/main/java/com/me/main/PhysicsEdApplication.java
@@ -0,0 +1,13 @@
+package com.me.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicsEdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysicsEdApplication.class, args);
+	}
+
+}
